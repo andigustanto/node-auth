c458314603dc54b354d421b4089d53dcabab21d2
andigustanto <andigustanto@gmail.com>
2022-04-07 23:02:26 +0700

    prepare cookie

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 //generate table
 // import Users from "./models/UserModel.js";
+
+dotenv.config();
 const app = express();
 
 try {
@@ -16,6 +20,7 @@ try {
     console.error(error);
 }
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(router);
 
