580c9ee24d483c43819d4df87d8f20eedaa1cab1
Andi Gustanto Mucharom <andigustanto@gmail.com>
2022-08-15 15:19:22 +0700

    reconfig

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('auth_db', 'root', '', {
+const db = new Sequelize('theworker2022', 'root', '', {
     host: "localhost",
     dialect: "mysql"
 });
